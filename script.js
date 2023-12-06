aab59b87124abceeff1d77995c860fcc72243d2d
OomeerAzzooaz <143521789+OomeerAzzooaz@users.noreply.github.com>
2023-12-06 11:22:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeColor(squareId){
+    var square = document.getElementById(squareId)
+    var randomColor = getRandomColor()
+    
+}
+function getRandomColor(){
+    var letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
